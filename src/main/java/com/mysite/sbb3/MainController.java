448a6d0c40e8d16af0e20d67b80139e19c218208
jngs-park <dear.jayp@gmail.com>
2022-10-17 01:31:04 +0900

    객체 조립시에 path variable 도 사용

--- src/main/java/com/mysite/sbb3/MainController.java
+++ src/main/java/com/mysite/sbb3/MainController.java
@@ -182,7 +182,7 @@ public class MainController {
         return p;
     }
 
-    @GetMapping("addPerson")
+    @GetMapping("/addPerson/{id}")
     @ResponseBody
     Person addPerson(Person p) {
         return p;
